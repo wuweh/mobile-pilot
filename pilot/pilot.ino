d9465b0f73ec162304ce796da332d8c88a21e19a
Liu Yang <leonliuyang@qq.com>
2015-11-30 10:29:00 +0800

    LCD Driver

--- pilot/pilot.ino
+++ pilot/pilot.ino
@@ -1,4 +1,8 @@
 #include "padroid.h"
+#include <Wire.h>   
+#include "LiquidCrystal_I2C.h"
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 static void _on_leftpad(int x, int y) {
   Serial.print("L: ");
@@ -31,6 +35,14 @@ static void _on_start_button(int s) {
 }
 
 void setup() {
+  lcd.init();                      // initialize the lcd 
+ 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.print("Hello, world!");
+  lcd.setCursor(3,1);
+  lcd.print("IEG_TEST");
+  
   pd_setup();
   
   pd_set_callback(_on_leftpad, _on_rightpad, _on_select_button, _on_start_button);
